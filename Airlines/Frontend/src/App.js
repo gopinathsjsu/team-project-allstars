f93820465295e58a9f3b6516c0703ff3cb166c13
payalghule <payalghule@gmail.com>
2021-11-22 15:48:48 -0800

    Sign up page for user and employee

--- Airlines/Frontend/src/App.js
+++ Airlines/Frontend/src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import SigninPage from "./pages/signin";
+import SignUpPage from "./pages/signup";
 function App() {
-    return (
-        <div className="App">
-            <Router>
-                <Switch>
-                    <Route exact path="/signin" component={SigninPage}></Route>
-                </Switch>
-            </Router>
-        </div>
-    );
+	return (
+		<div className="App">
+			<Router>
+				<Switch>
+					<Route exact path="/signin" component={SigninPage}></Route>
+					<Route exact path="/signup" component={SignUpPage}></Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
