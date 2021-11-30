1a7b71c31459cd4eecb40454f20cc71512fbc810
madhuridv <madhuridv3@gmail.com>
2021-11-30 04:29:25 -0800

    Merge branch 'main' of https://github.com/gopinathsjsu/team-project-allstars

--- Airlines/Frontend/src/App.js
+++ Airlines/Frontend/src/App.js
@@ -6,28 +6,43 @@ import DashBoardPage from "./pages/dashboard";
 import SearchResultsPage from "./pages/searchresults";
 import LandingPage from "./pages/landing";
 import "bootstrap/dist/css/bootstrap.min.css";
-function App() {
-    return (
-        <div className="App">
-            <Router>
-                <Switch>
-                <Route exact path ='/' component={LandingPage}></Route>
-                    <Route exact path="/signin" component={SigninPage}></Route>
-                    <Route exact path="/signup" component={SignUpPage}></Route>
-                    <Route
-                        exact
-                        path="/dashboard"
-                        component={DashBoardPage}
-                    ></Route>
-                    <Route
-                        exact
-                        path="/searchresults"
-                        component={SearchResultsPage}
-                    ></Route>
-                </Switch>
-            </Router>
-        </div>
-    );
-}
+import EmployeeDashboard from "./pages/employeeDashboard";
+import FlightAddition from "./pages/addFlight";
+import TravellerInfoPage from "./pages/travellerInfo";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+function App() {
+	return (
+		<div className="App">
+			<Router>
+				<Switch>
+					<Route exact path="/" component={LandingPage}></Route>
+					<Route exact path="/signin" component={SigninPage}></Route>
+					<Route exact path="/signup" component={SignUpPage}></Route>
+					<Route exact path="/dashboard" component={DashBoardPage}></Route>
+					<Route
+						exact
+						path="/searchresults"
+						component={SearchResultsPage}
+					></Route>
+					<Route
+						exact
+						path="/travellerinfo"
+						component={TravellerInfoPage}
+					></Route>
+					<Route
+						exact
+						path="/employee/dashboard"
+						component={EmployeeDashboard}
+					></Route>
+					<Route
+						exact
+						path="/employee/addFlight"
+						component={FlightAddition}
+					></Route>
+				</Switch>
+			</Router>
+		</div>
+	);
+}
 export default App;
